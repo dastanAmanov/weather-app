fb4555516618c25a0a4a7bf8e8f774e8f0496b96
dastan amanov <dastanamanov007@gmail.com>
2022-03-02 22:49:55 +0500

    change js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 let searchBox = document.querySelector("#searchinput");
-
-searchBox.addEventListener("keypress", setQuery);
-
+let date = document.querySelector('.date')
+console.log(date);
 function setQuery(e){
     if(e.keyCode == 13){
-        getResults(searchBox.value)
+        getResults(searchBox.value.trim())
     }
 }
 let api = {
@@ -16,19 +15,29 @@ function getResults(query){
     .then(weather => weather.json())
     .then(displayResults);
 }
+searchBox.addEventListener("keypress", setQuery);
 
-function displayResults(weather){
+let dateBulder = (d) =>{
+    let months = ['Yanvar', 'Febral', 'Mart', 'Aprel', 'May', 'Iyun', 'Iyul', 'Avgust', 'Sentyabr', 'Oktyabr', 'Noyabr', 'Dekabr'];
+    let days = ['Dushanba', 'Seshanba', 'Chorshanba', 'Payshanba', 'Juma', 'Shanba', 'Yakshanba']
+    let month = months[d.getMonth()];
+    let day = days[d.getDay()];
+    let dates = d.getDate();
+    let year = d.getFullYear();
+    date.textContent = `${day}, ${dates}-${month}, ${year}-yil`
+}
+dateBulder(new Date())
 
-    let title = document.querySelector("#title")
-    let tempinfo = document.querySelector("#tempinfo")
-    let maxtemp = document.querySelector("#maxtemp")
-    let mintemp = document.querySelector("#mintemp")
+function displayResults(weather) {
+    let title = document.querySelector("#title");
+    let tempinfo = document.querySelector("#tempinfo");
+    let maxtemp = document.querySelector("#maxtemp");
+    let mintemp = document.querySelector("#mintemp");
 
     title.textContent = weather.name;
     tempinfo.textContent = Math.round(weather.main.temp);
     maxtemp.textContent = Math.round(weather.main.temp_max);
     mintemp.textContent = Math.round(weather.main.temp_min)
 }
-
 
 
